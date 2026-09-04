362c7eb18bfcc6e6e7e1b8185aca6ebb1cf4cee7
Nishant <142768754+NishCode17@users.noreply.github.com>
2026-09-04 12:10:15 +0530

    Add: task-scheduling - submission-2

---
+++ Data Structures & Algorithms/task-scheduling/submission-2.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        HashMap<Character,Integer> map=new HashMap<>();
+
+        for (char task : tasks) {
+            map.put(task, map.getOrDefault(task, 0) + 1);
+        }
+
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->Integer.compare(b,a));
+
+        for (int count : map.values()) {
+            pq.offer(count);
+        }
+
+        Queue<int[]> queue=new LinkedList<>();
+        int time=0;
+
+        while(!pq.isEmpty() || !queue.isEmpty()){
+            if(!pq.isEmpty()){
+                int freq=pq.poll();
+                freq--;
+                if(freq>0){
+                    queue.offer(new int[]{freq,time+n+1});
+                }
+            }
+
+            time++;
+
+            while(!queue.isEmpty() && queue.peek()[1] == time){
+                int f=queue.poll()[0];
+                pq.offer(f);
+            }
+            
+        }
+
+        return time;
+    }
+}
